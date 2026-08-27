7eace2b8f666fb57ab0564cbe4e139d1f9d9b536
wonders1899 <2.03917651e+08+wonders1899@users.noreply.github.com>
2026-08-27 11:38:11 +0000

    Fix creator feed migration timestamp

--- api/src/database/migrations/1788000000000-AddCreatorFeed.ts
+++ api/src/database/migrations/1788000000000-AddCreatorFeed.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddCreatorFeed1788000000000 implements MigrationInterface {
-  name = "AddCreatorFeed1788000000";
+  name = "AddCreatorFeed1788000000000";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
